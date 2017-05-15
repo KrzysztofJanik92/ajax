a680e8234e9c1bf3beb7fa5fda9074e58a049a50
KrzysztofJanik92 <krzysztof.janik@vp.pl>
2017-05-15 21:02:01 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,135 +1,84 @@
 var $body = $('body');
-   var $wikiElem = $('#wikipedia-links');
-   var $wikiHeader = $('#wikipedia-header');
-   var $nytHeaderElem = $('#nytimes-header');
-   var $nytElem = $('#nytimes-articles');
-   var $greeting = $('#greeting');
-   var $error1 = $('#error1'); 
+var $wikiElem = $('#wikipedia-links');
+var $wikiHeader = $('#wikipedia-header');
+var $nytHeaderElem = $('#nytimes-header');
+var $nytElem = $('#nytimes-articles');
+var $greeting = $('#greeting');
+var $error1 = $('#error1'); 
 
-$( document ).ready(function() {
-   $wikiElem.text("");
-   $nytElem.text("");
-    });
-	
+
+$(document).ready(function() {
+	$wikiElem.text("");
+	$nytElem.text("");
+});
+
 function loadData(){
-   var streetStr = $('#street').val();
-   var cityStr = $('#city').val();
-   var address = streetStr + ', ' + cityStr;
-   $greeting.text('So, you want to live at ' + address + '?');
-   var temp = getData(streetStr, cityStr);
-   if(temp.errorValue == true){
-	   console.log(temp.errorMessage);
-      $error1.text("Fields cannot be empty");
-   }
-   
-   
-   
-}
-
-function getData(streetField, cityField){
-		if(streetField.length == 0 || cityField.length == 0)
-		{
-			return {
-				errorValue: true,
-				errorMessage: "Pola nie mogą być puste"
-			};
-		}
-		else
-		{
-
-			$body.css("background-image", "url('"+ getGoogleData(streetField+', '+cityField)+"')");
-			$body.css("background-repeat", "no-repeat");
-			$body.css("background-size", "cover");
-			
-			getWikiData(cityField);
-			getNyTData(cityField);
-
-			return{
-				errorValue: false,
-			};
-			
-		}
+	var streetStr = $('#street').val();
+	var cityStr = $('#city').val();
+	var address = streetStr + ', ' + cityStr;
 	
+	if(streetStr.length != 0 || cityStr.length != 0){
+		$greeting.text('So, you want to live at ' + address + '?');
+		getGoogleData(address);
+		getData(cityStr);
+	}
+	else
+	{
+		$error1.text("Fields cannot be empty");
+	}
 }
 
 function getGoogleData(adress){
-	return 'https://maps.googleapis.com/maps/api/streetview?size=600x400&key=AIzaSyDpFmKn0ixCuDZQ2m8wLrp0Az0TWZlgyr4&location=' + adress + '';
-	
+	var link = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&key=AIzaSyDpFmKn0ixCuDZQ2m8wLrp0Az0TWZlgyr4&location=' + adress + '';
+	$body.css("background-image", "url('"+ link +"')");
 }
-function getWikiData(city){
-	var url = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&format=json"+"&search="+city;
+
+var fetchData = function(query, dataUrl){
+	return $.ajax({
+		url: dataUrl,
+		data: query,
+		dataType: 'json',
+	});
+}
+
+function getData(city){
+var getWiki = fetchData(
+	{
+		'search':city,
+		'origin':'*',
+		'action':'opensearch',
+		'format':'json',
+	}, 'https://en.wikipedia.org/w/api.php'),
+	getNYT = fetchData(
+	{
+		'q': city,
+		'api-key': '4985d80ad9234d0ea918b29c236b8b58',
+	}, 'https://api.nytimes.com/svc/search/v2/articlesearch.json');	
 	
+	$.when(getWiki,getNYT).done(function(wikiData,nytData){
+		var temp = wikiData[0];
+		temp = temp[1];
+		for (var i = 0; i < temp.length; i++) 
+		{
+			articleStr = temp[i];
+			var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+			$wikiElem.append('<li><a href="'+url+'">'+articleStr+'</a></li>');
+		}
 		
-         $.ajax({
-            url: url,
-           data: 'query',
-           dataType: 'json',
-           type: 'POST',
-           headers: { 'Api-User-Agent': 'Example/1.0' },
-           origin: 'https://jsfiddle.net/',
-
-
-           success: function (data) {  
-                links = data[1];
-				wikiCallback(false, links);
-		   },
-			error: function(data){
-				wikiCallback(true,data);
-			}
-            });
-			
-}
-
-function getNyTData(city){
-	
-	$.ajax({
-         'type': 'GET',
-         'url': 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
-         data:{
-            'q': city,
-            'response-format': "jsonp",
-            'api-key': '4985d80ad9234d0ea918b29c236b8b58',
-            'callback': 'svc_search_v2_articlesearch'
-            },
-            success: function(data){
-               nytCallback(false, data.response.docs, city);
-            },
-			error: function(data){
-				nytCallback(true,data);
-			}
-			
-       });
-	   
-}
-function wikiCallback(error,data, city){
-	if(error == true){
-       $error1.text("wikipedia connection error");
-	}
-	else{
-		
-               for (var i = 0; i < data.length; i++) 
-                   {
-                       articleStr = data[i];
-                       var url = 'https://en.wikipedia.org/wiki/' + articleStr;
-                       $wikiElem.append('<li><a href="'+url+'">'+articleStr+'</a></li>');
-
-                   }
-               
-
-                   
-	}
-}
-
-function nytCallback(error,data){
-	if(error == true){
-       $error1.text("New York Times connection error");
-	}
-	else{
+		temp = nytData[0];
+		temp = temp.response.docs;
 		$nytHeaderElem.text('New York Times Articles about ' + city);
 		for (var i = 0; i < 5; i++) 
-                   {
-                       var article = data[i];
-                       $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+ '<p>' + article.snippet + '</p>'+ '</li>');             
-                   }
-	}
-}
+		{
+			var article = temp[i];
+			$nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+ '<p>' + article.snippet + '</p>'+ '</li>');             
+		}
+	});
+	
+	$.when(getWiki).fail(function(Data){
+		$error1.text("wikipedia connection error");
+	});
+	$.when(getNYT).fail(function(Data){
+		$error1.text("New York Times connection error");
+	});
+}	
